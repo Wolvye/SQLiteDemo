197b8a6acc9873785e8354f227e9c8bafd725f02
Wolvye <alainsoltau@googlemail.com>
2025-07-21 11:51:20 +0200

    Update

--- SQLiteDemo/MVVM/Models/Customer.cs
+++ SQLiteDemo/MVVM/Models/Customer.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlTable = SQLite.TableAttribute;
+using SqlColumn = SQLite.ColumnAttribute;
+
 
 namespace SQLiteDemo.MVVM.Models
 {
+    [SqlTable("Customers")]
     public class Customer
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [SqlColumn("name"), Indexed, NotNull]
 
         public string Name { get; set; }
+        [Unique]
         public string Phone { get; set; }
+        
         public int Age { get; set; }
+        [MaxLength(100)]
         public string Address { get; set; }
+        [Ignore]
+
+        public bool IsYoung =>
+            Age > 50 ? true : false;
     }
 }
